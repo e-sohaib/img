236ee4e416b9beed899b212ac3856e678df0cbe1
sohaib <sohaib1234321@gmail.com>
2024-12-23 16:58:22 +0330

    bot

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import noise
+import fingerprint.noise2
+import feature_maching
+import template_maching
+import fingerprint.fingerprint
+
+if __name__ == "__main__":
+    print("done!")
+
---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+
+with open("/mnt/imageproccess/p.txt" , 'r') as r:
+    token = r.read()
+    
+bot = telebot.TeleBot(token=token)
+@bot.message_handler(commands=['start'])
+def start(message) :
+    bot.reply_to(message , "hi")
+    
+bot.polling(non_stop=True , timeout=25)
+    
