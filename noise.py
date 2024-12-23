b1767d31f7b6ef5ac07b02af3a4cf03a8e626167
sohaib <sohaib1234321@gmail.com>
2024-12-23 17:38:32 +0330

    bot

--- noise.py
+++ noise.py
@@ -1,15 +1,14 @@
 import numpy as np
 import cv2 as cv
 
-
-image = cv.imread('2.jpg' , 0)
-res , tresh = cv.threshold(image ,50 ,255 , cv.THRESH_OTSU)
-
-ker = np.ones((2,2) , dtype= 'uint8')
-result = cv.morphologyEx(tresh , cv.MORPH_RECT, ker )
-
-
-cv.imwrite('result.jpg',tresh)
-
-cv.waitKey(0)
-cv.destroyAllWindows()
+def noise():
+    image = cv.imread('2.jpg' , 0)
+    res , tresh = cv.threshold(image ,50 ,255 , cv.THRESH_OTSU)
+    
+    ker = np.ones((2,2) , dtype= 'uint8')
+    result = cv.morphologyEx(tresh , cv.MORPH_RECT, ker )
+    
+    
+    cv.imwrite('result.jpg',tresh)
+    
+    
